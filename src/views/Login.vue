13cb72da9fced7e8e06e0b3b25bb725cd7bdc139
codingjc <996186701@qq.com>
2021-06-22 23:35:10 +0800

    202106222334:web第一次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <el-form :rules="roles" ref="loginForm" :model="loginForm" class="loginContainer">
+      <h3 class="loginTitle">系统登陆</h3>
+
+      <el-form-item prop="username">
+        <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+
+      <el-form-item prop="password" >
+        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码" @keydown.enter.native="submitLogin"></el-input>
+      </el-form-item>
+
+      <el-checkbox class="loginRemember" v-model="checked"></el-checkbox>
+
+      <el-button type="primary" style="width: 100%;" @click="submitLogin">登陆</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        loginForm:{
+          username: 'admin',
+          password: '123'
+        },
+        roles: {
+          username: [{required: true, message:'请输入用户名', trigger: 'blur'}],
+          password: [{required: true, message:'请输入密码', trigger: 'blur'}]
+        },
+        checked: true
+      }
+    },
+    methods: {
+      submitLogin(){
+        this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            this.$message.error('请输入用户名和密码!');
+            return false;
+          }
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+   .loginContainer {
+     border-radius: 15px;
+     background-clip: padding-box;
+     margin: 180px auto;
+     width: 350px;
+     padding: 15px 35px 15px 35px;
+     background: #fff;
+     border: 1px solid #eaeaea;
+     box-shadow: 0 0 25px #cac6c6;
+   }
+   .loginTitle{
+     margin: 15px auto 20px auto;
+     text-align: center;
+     color: #505458;
+   }
+   .loginRemember{
+     text-align: left;
+     margin-bottom: 15px;
+   }
+</style>
